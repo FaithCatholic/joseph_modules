d2fe59a3fb09f407614ca216a059dab3f3d14428
Joseph Novembre <josephnovembre@Josephs-MacBook-Pro-2.local>
2024-05-28 10:14:18 -0400

    twig

--- masstimes/src/massTimesService.php
+++ masstimes/src/massTimesService.php
@@ -48,15 +48,81 @@ class massTimesService {
         
         $url = 'https://apiv4.updateparishdata.org/Churchs/?lat='.trim($latitude).'&long='.trim($longitude).'&pg=1';
         
+        $json_data = '{ "church_address_city_name" : "Puchenstuben",
+            "church_address_country_territory_name" : "Austria",
+            "church_address_county" : null,
+            "church_address_postal_code" : "3214",
+            "church_address_providence_name" : "Lower Austria",
+            "church_address_street_address" : "5 Puchenstuben Rd.",
+            "church_type_name" : "Parish",
+            "church_worship_times" : [ { "comment" : "",
+                  "day_of_week" : "99",
+                  "id" : "935441",
+                  "is_perpetual" : false,
+                  "language" : "English ",
+                  "service_typename" : "Devotions",
+                  "time_end" : "1/1/1900 12:00:00 AM",
+                  "time_start" : "1/1/1900 12:00:00 AM"
+                },
+                { "comment" : "",
+                  "day_of_week" : "0",
+                  "id" : "935442",
+                  "is_perpetual" : false,
+                  "language" : "German ",
+                  "service_typename" : "Weekend",
+                  "time_end" : null,
+                  "time_start" : "1/1/1900 10:00:00 AM"
+                },
+                { "comment" : "Nov - Dec",
+                  "day_of_week" : "6",
+                  "id" : "1250169",
+                  "is_perpetual" : false,
+                  "language" : "German ",
+                  "service_typename" : "Weekend",
+                  "time_end" : "1/1/1900 12:00:00 AM",
+                  "time_start" : "1/1/1900 5:00:00 PM"
+                }
+              ],
+            "comments" : "",
+            "diocese_name" : "Sankt Pollten",
+            "diocese_type_name" : null,
+            "directions" : "",
+            "distance" : "21.413402072542",
+            "email" : "bo.stpoelten@kirche.at",
+            "id" : "5634",
+            "language_name" : "German ",
+            "last_update" : "3/21/2012 3:00:30 AM",
+            "lat_long_source" : null,
+            "latitude" : "47.9299467",
+            "longitude" : "15.2618797",
+            "military_time" : true,
+            "name" : "St.Ann",
+            "pastors_name" : " ",
+            "phone_number" : "0748248201",
+            "rite_type_name" : "Roman-Latin",
+            "url" : "www.kirche.at/stpoelten/pfarren/pfarrinfo.php?links=Puchenstuben",
+            "wheel_chair_access" : true
+          }';
+
+          /*$data = json_decode($json_data, TRUE);
+
+          return [
+            '#theme' => 'churchinfo',
+            '#church_data' => $data,
+        
+        ];*/
         try {
             
             $response = $this->httpClient->request('GET', $url );
     
              $data = json_decode($response->getBody()->getContents(), TRUE);
+             $data1 = $data[0];
+
+
             return [
-            '#type' => 'table',
-            '#rows' => $data[0]['church_worship_times'],            
-            '#title' => ('API Data'),
+                '#theme' => 'churchinfo',
+                '#church_data' => $data1,
+            
         ];
   
 
